f014b54a571f1bd30576133baefea9c968229f79
Kevin Meredith <kevin.m.meredith@gmail.com>
2016-09-18 23:06:05 -0400

    proof of concept>

--- README.md
+++ README.md
@@ -29,7 +29,12 @@ Example: `Foo.json`
 
 source: http://json-schema.org/examples.html
 
-Output:
+## Ideal Solution
+
+Given the above JSON Schema, output a `Foo.scala` file by:
+
+* reading the JSON Schema into an `OutputClass` class, i.e. which captures the key-value fields to be 
+  used for the generated case class, e.g. `Foo` below
 
 `case class Foo(firstName: String, lastName: String, age: Option[BoundedInt])`
 
@@ -42,3 +47,50 @@ object BoundedInt {
 	 if(x >= 0) Some( new BoundedInt(x) ) else None
 }
 ```
+
+## Current Proof of Concept
+
+```
+> test:run
+[info] Compiling 1 Scala source to C:\Users\kevin\workspace\map_to_case_class\target\scala-2.11\classes...
+[info] Compiling 1 Scala source to C:\Users\kevin\workspace\map_to_case_class\target\scala-2.11\classes...
+[info] Compiling 1 Scala source to C:\Users\kevin\workspace\map_to_case_class\target\scala-2.11\test-classes...
+[info] Running net.Test
+lines:
+: {
+        "title": "Example Schema",
+        "type": "JObj",
+        "properties": {
+                "firstName": {
+                        "_type": "JStr"
+                },
+                "lastName": {
+                        "_type": "JStr"
+                },
+                "age": {
+                        "description": "Age in years",
+                        "_type": "JInt",
+                        "minimum": 0
+                }
+        },
+        "required": ["firstName", "lastName"]
+}
+output: Some(OutputClass(Example Schema,Map(age -> MyInt(Some(0),None), lastName -> MyStr, firstName -> MyStr),List(firstName, lastName)))
+[success] Total time: 6 s, completed Sep 19, 2016 3:00:56 AM
+```
+
+### Notes
+
+I'm cheating by using `_type` as the key name.
+
+## Conclusion
+
+Although the `OutputClass` to `.scala` file is not shown here, I believe that it'll be simple to implement:
+
+```
+def f(outputClass: OutputClass): Unit 
+```
+
+which, given an `OutputClass`, produces a `.scala` file.
+
+Lastly, as a side thought, I believe that I can test that the output `$FILE.scala` compiles via ScalaTest.
--- src/main/scala/net/JsonSchema.scala
+++ src/main/scala/net/JsonSchema.scala
@@ -17,17 +17,37 @@ sealed trait JsonType
 case object JStr  extends JsonType
 case object JInt extends JsonType
 
+// TODO: Use a `JString` decoder rather than whatever's given by default
 object JsonType {
-	implicit val jsonTypeDecoder: Decoder[JsonType] = deriveDecoder[JsonType]
+	def read(x: String): Option[JsonType] = {
+		if      (x == "JStr") Some(JStr)
+		else if (x == "JInt") Some(JInt)
+		else 	None
+	}
 }
 
-case class Property(name: String, _type: JsonType, description: Option[String], minimum: Option[Int], maximum: Option[Int])
-case class JsonSchema(title: String, _type: RootJsonType, properties: List[Property], requiredFields: List[String])
-
+case class Property(key: String, _type: JsonType, description: Option[String], minimum: Option[Int], maximum: Option[Int])
+case class PartialProperty(_type: String, description: Option[String], minimum: Option[Int], maximum: Option[Int])
 case class OutputClass(name: String, fields: Map[String, MyType], required: List[String])
 
+object PartialProperty {
+	implicit val propertyDecoder: Decoder[PartialProperty] = deriveDecoder[PartialProperty]
+}
+
 object Property {
-	implicit val propertyDecoder: Decoder[Property] = deriveDecoder[Property]
+
+	type AccType = Option[List[Property]]
+
+	def read(pairs: Map[String, Json]): Option[List[Property]] = {
+		pairs.foldLeft[AccType](Some(List.empty)){ (acc, elem) =>
+			val (key, obj) = elem
+			for {
+				currentAcc <- acc
+				partial    <- obj.as[PartialProperty].toOption
+				keyType   <- JsonType.read(partial._type)
+			} yield Property(key, keyType, partial.description, partial.minimum, partial.maximum) :: currentAcc
+		}
+	}
 }
 
 sealed trait MyType
@@ -36,27 +56,27 @@ case object MyStr            				         extends MyType
 
 object JsonMapper {
 
-	implicit val decoder: Decoder[JsonSchema] = deriveDecoder[JsonSchema]
+	def cursor(json: Json): HCursor = json.hcursor
 
-	def read(x: String): Option[OutputClass] = 
-		parse(x).toOption >>= (json => json.as[JsonSchema].toOption) >>= readHelper
-
-	private def readHelper(schema: JsonSchema): Option[OutputClass] = schema._type match {
-		case JObj => 
-			val name                        = schema.title	
-			val outputClassFields: Map[String, MyType] = schema.properties.map(extractField).toMap
-			Some( OutputClass(name, outputClassFields, schema.requiredFields) )
-		case JArr => None
+	def read(str: String): Option[OutputClass] =  {
+		for {
+			json       <- parse(str).toOption
+			jsonCursor <- Some( cursor(json) )
+			title      <- jsonCursor.downField("title").as[String].toOption
+			_type      <- jsonCursor.downField("type").as[String].toOption
+			_          <- if(_type == "JObj") Some( () ) else None  
+			jsonProps  <- jsonCursor.downField("properties").as[Map[String, Json]].toOption
+			required   <- jsonCursor.downField("required").as[List[String]].toOption
+			properties <- Property.read(jsonProps)
+		} yield OutputClass(title, properties.map(extractField).toMap, required)
 	}
 
-	private def extractField(property: Property): (String, MyType) = {
-		val fieldName = property.name
+	private def extractField(property: Property): (String, MyType) = 
 		property._type match {
-			case JStr  => (fieldName, MyStr)
-			case JInt  => (fieldName, MyInt(property.minimum, property.maximum))
+			case JStr  => (property.key, MyStr)
+			case JInt  => (property.key, MyInt(property.minimum, property.maximum))
 			case _ 	   => ??? // TODO: extract array or object as a List and another case class, respectively, maybe?
 		} 
-	}
 }
 
 case class NotInBound(min: Int, max: Int, value: Int)
